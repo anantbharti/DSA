9c578ea432f81602e77de229a34caeb45c718e84
Anant Bharti <57601502+anantbharti@users.noreply.github.com>
2021-08-25 01:42:21 +0530

    minimum spanning tree

---
+++ Graphs/Prim's Algorithm.cpp
@@ -0,0 +1,73 @@
+/*
+ * To find minimum spanning tree of a graph.
+ * Time complexity :- O(V*V*logE)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+vector<pair<int,int>> adj[101];
+priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+vector<bool> vis;
+vector<vector<int>> mst;
+int n,no_of_nodes;
+int ans;
+
+void dfs(int node,int par)
+{
+    if(no_of_nodes==n)return;
+    no_of_nodes++;
+    vis[node]=true;
+    
+    for(auto it:adj[node])
+    {
+        if(!vis[it.first])
+        pq.push({it.second,{node,it.first}});
+    }
+    while(!pq.empty())
+    {
+        int u=pq.top().second.first;
+        int v=pq.top().second.second;
+        int d=pq.top().first;
+        pq.pop();
+        if(vis[v])
+        continue;
+        else
+        {
+            mst.push_back({u,v,d});
+            ans+=d;
+            dfs(v,node);
+            break;
+        }
+    }
+}
+
+typedef pair<int,int> pii;
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    n;cin>>n;  // no of nodes
+    int e;cin>>e;  // no of edges
+    while(e--)
+    {
+        int x,y;   // nodes
+        cin>>x>>y;
+        int d;cin>>d;
+        adj[x].push_back({y,d});
+        adj[y].push_back({x,d});
+    }
+    vis.assign(n,false);
+    ans=0;
+    no_of_nodes=0;
+    dfs(0,-1);
+
+    cout<<ans<<"\n";
+    for(auto it:mst)
+    {
+        cout<<it[0]<<" "<<it[1]<<" "<<it[2]<<"\n";
+    }
+
+    return 0;
+}
